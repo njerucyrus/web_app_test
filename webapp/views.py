c075a4186181c173dad8c4b69af651d873a1e17f
Njeru Cyrus <njerucyrus5@gmail.com>
2020-03-12 18:29:31 +0300

    added login view

--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -32,3 +33,42 @@ class RegisterView(View):
             redirect_route = '/login/'
             messages.success(request, 'Account created successfully. Login to proceed')
             return HttpResponseRedirect(redirect_to=redirect_route)
+
+
+class LoginView(View):
+    template_name = 'login.html'
+
+    def get(self, request, *args, **kwargs):
+        next_url = request.GET.get('next', '').strip()
+        user = request.user
+        if user.is_authenticated and next_url != '':
+
+            return HttpResponseRedirect(next_url)
+        elif user.is_authenticated and next_url != '':
+
+            return HttpResponseRedirect('/')
+        else:
+            return render(request, template_name=self.template_name, context={'request': request})
+
+    def post(self, request, *args, **kwargs):
+        next_url = request.GET.get('next', '').strip()
+        data = request.POST
+        user = authenticate(request=request, username=data.get('username'),
+                            password=data.get('password'))
+        if user is not None:
+            if user.is_active:
+                request.session['phone_number'] = data.get('phone_number')
+                login(request, user)
+                if next_url != '':
+                    messages.success(request, 'Login was successful')
+
+                    return HttpResponseRedirect(next_url)
+                else:
+                    messages.success(request, 'Login was successful')
+                    return HttpResponseRedirect('/')
+            else:
+                messages.error(request, 'Invalid username/password')
+                return render(request, template_name=self.template_name, context={'request': request})
+        else:
+            messages.error(request, "Invalid username/password")
+            return render(request, template_name=self.template_name, context={'request': request})
